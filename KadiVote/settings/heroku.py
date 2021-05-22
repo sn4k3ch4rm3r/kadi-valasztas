9dbbe6cd111fb1be5560c3b9c93cc99212f4b95b
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2021-05-22 15:49:57 +0200

    Hopefully this will fix the db on heroku

--- KadiVote/settings/heroku.py
+++ KadiVote/settings/heroku.py
@@ -1,8 +1,14 @@
+from KadiVote.settings.dev import DATABASES
 from .base import *
 import django_heroku
+import dj_database_url
 
 DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = ['kadi-vote.herokuapp.com']
 
 django_heroku.settings(locals())
+
+DATABASES = {
+	'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
