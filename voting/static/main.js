f29cb7c347fdc6982768c2cf0b3f32cc273b8521
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2021-05-22 23:53:39 +0200

    Voting page mobile design

--- voting/static/main.js
+++ voting/static/main.js
@@ -1,4 +1,16 @@
-var ripple = document.querySelectorAll(".ripple");
+var ripple = document.querySelectorAll(".ripple, .mdc-ripple-surface");
 ripple.forEach(element => {
 	mdc.ripple.MDCRipple.attachTo(element);
+});
+
+var candidateCards = document.querySelectorAll('.voting-option');
+candidateCards.forEach(card => {
+	card.addEventListener("click", e => {
+		card.querySelector('.voting-option__input').checked = true;
+		try {
+			document.querySelector('.selected').classList.remove('selected');
+		}
+		catch {}
+		card.classList.add('selected');
+	});
 });
