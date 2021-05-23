7c3faed4f5f6ff5130baec933ca3d7b5a151fb2a
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2021-05-23 10:22:00 +0200

    Typo

--- voting/urls.py
+++ voting/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 	path('', views.LandingPage.as_view(), name='landing'),
 	path('auth/', views.Authenticate.as_view(), name='auth'),
 	path('vote/', views.Vote.as_view(), name='vote'),
-	path('howitowrks/', TemplateView.as_view(template_name='voting/howitworks.html'), name='howitworks'),
+	path('howitworks/', TemplateView.as_view(template_name='voting/howitworks.html'), name='howitworks'),
 	path('logout/', views.logout, name='logout'),
 ]
