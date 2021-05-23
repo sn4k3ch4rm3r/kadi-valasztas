07440006d4727011e1e80c011dc8836231127c51
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2021-05-23 17:25:28 +0200

    Added models for voting

--- voting/admin.py
+++ voting/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import KadiCandidate
+from .models import KadiCandidate, Vote, Voter
 
 admin.site.register(KadiCandidate)
+admin.site.register(Vote)
+admin.site.register(Voter)
--- voting/models.py
+++ voting/models.py
@@ -1,12 +1,20 @@
 from os import truncate
 from django.db import models
+from django.db.models.fields import CharField
 
 class KadiCandidate(models.Model):
 	firstname = models.CharField(max_length=10)
 	lastname = models.CharField(max_length=40)
 	nickname = models.CharField(max_length=20, blank=True)
-	classname = models.CharField(max_length=1)
+	classname = models.CharField(max_length=1, primary_key=True)
 	imageurl = models.CharField(max_length=20)
 
 	def __str__(self):
 		return self.lastname + ' ' + self.firstname + ' - 11. ' + self.classname
+
+class Vote(models.Model):
+	classname = models.ForeignKey(KadiCandidate, on_delete=models.DO_NOTHING)
+
+class Voter(models.Model):
+	email = models.CharField(max_length=100, primary_key=True)
+	refresh_token = models.TextField()
