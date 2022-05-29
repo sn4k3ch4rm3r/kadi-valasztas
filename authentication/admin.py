9467b9b565333957ca7910a765a4fd3c21b473aa
Boldizsár Tóth <39193138+sn4k3ch4rm3r@users.noreply.github.com>
2022-05-29 13:46:42 +0200

    Create users without password

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,28 +1,41 @@
 from django.contrib import admin
-from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from django.contrib.auth.models import Group as DjangoGroup
-from django.contrib.auth.forms import PasswordResetForm
-from django.utils.crypto import get_random_string
 from .models import User, Group
 
 class MyUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = User
 
+class MyUserCreationForm(UserCreationForm):
+	def __init__(self, *args, **kwargs):
+		super(UserCreationForm, self).__init__(*args, **kwargs)
+		self.fields['password1'].required = False
+		self.fields['password2'].required = False
+
+	def save(self, commit=True):
+		user = super(MyUserCreationForm, self).save(commit=False)
+		user.set_unusable_password()
+
+		if commit:
+			user.save()
+		
+		return user
+
 class MyUserAdmin(UserAdmin):
 	form = MyUserChangeForm
-	
+	add_form = MyUserCreationForm
 	add_fieldsets = (
 		(None, {
 			'classes': ('wide',),
-			'fields': ('email', 'password1', 'password2'),
+			'fields': ('email',),
 		}),
 		('Jogosultságok', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
 	)
 	
 	fieldsets = (
-		(None, {'fields': ('email', 'display_name', 'password')}), 
+		(None, {'fields': ('email', 'display_name')}), 
 		('Szavazás', {'fields': ('can_vote', 'has_voted')}),
 		('Jogosultságok', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}), 
 		('Fontos dátumok', {'fields': ('last_login', 'date_joined')}),
